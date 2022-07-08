7bbea020052421966cd35913796e1b8d7b5258a1
akshay <codenzatechnology@gmail.com>
2022-07-08 19:28:10 +0530

    'current_openings_reusable'

--- src/components/Common.js
+++ src/components/Common.js
@@ -3,7 +3,29 @@ import CountUp from "./CountUp";
 import { useInView } from "react-intersection-observer";
 
 const Info = () => {
-  const { ref:myRef, inView:infoVisible} = useInView();
+  const Data = [
+    {
+      name: "Clients",
+      number: 280,
+      timer: 10,
+    },
+    {
+      name: "Projects",
+      number: 32,
+      timer: 50,
+    },
+    {
+      name: "Won Awards",
+      number: 20,
+      timer: 50,
+    },
+    {
+      name: "Hard Workers",
+      number: 80,
+      timer: 20,
+    },
+  ];
+  const { ref: myRef, inView: infoVisible } = useInView();
   return (
     <section id="why-us" className="wow fadeIn">
       <div className="container">
@@ -12,31 +34,21 @@ const Info = () => {
           <p> 10+ Fortune 150 Companies</p>
         </header>
 
-        <div className="row counters" ref={myRef} >
-          {
-          infoVisible ? <>
-            <div className="col-lg-3 col-6 text-center counter-up">
-            <CountUp start={0} end={280} timer={10} />
-            <p>Clients</p>
-          </div>
-
-          <div className="col-lg-3 col-6 text-center counter-up">
-            <CountUp start={0} end={32} timer={50} />
-            <p>Projects</p>
-          </div>
-
-          <div className="col-lg-3 col-6 text-center counter-up">
-            <CountUp start={0} end={20} timer={50} />
-            <p>Won Awards</p>
-          </div>
-
-          <div className="col-lg-3 col-6 text-center counter-up">
-            <CountUp start={0} end={80} timer={20} />
-            <p>Hard Workers</p>
-          </div>
-
-          </> : ''
-        }
+        <div className="row counters" ref={myRef}>
+          {infoVisible ? (
+            <>
+              {Data.map(function (o, i) {
+                return (
+                  <div className="col-lg-3 col-6 text-center counter-up">
+                    <CountUp start={0} end={o.number} timer={o.timer} />
+                    <p>{o.name}</p>
+                  </div>
+                );
+              })}
+            </>
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </section>
--- src/components/Reusable.js
+++ src/components/Reusable.js
@@ -8,6 +8,136 @@ import Client6 from "../img/clients/client-6.png";
 import Client7 from "../img/clients/client-7.png";
 import Client8 from "../img/clients/client-8.png";
 
+export const Jobs = () => {
+  const JobData = [
+    {
+      title: "Selinium tester(Manual + Automation)",
+      icon: "ion-ios-world-outline",
+      color: "#fac19b",
+      ref: 385693,
+      exp: "3 - 5",
+      type: "Permanent",
+      location: "Pune",
+      department: "Services",
+      class: "offset-lg-1",
+    },
+    {
+      title: "Database Admin",
+      icon: "ion-ios-keypad-outline",
+      color: "#37c7a9",
+      ref: 38221493,
+      exp: "2 - 4",
+      type: "Permanent",
+      location: "Pune",
+      department: "Maintaince",
+      class: "",
+    },
+    {
+      title: "Scrum Master",
+      icon: "ion-grid",
+      color: "#ff689b",
+      ref: 385693,
+      exp: "3 - 5",
+      type: "Permanent",
+      location: "Pune",
+      department: "Services",
+      class: "offset-lg-1",
+    },
+    {
+      title: "UI UX Designer",
+      icon: "ion-ios-world-outline",
+      color: "#e9bf06",
+      ref: 3781493,
+      exp: "2 - 3",
+      type: "Permanent",
+      location: "Pune",
+      department: "Maintaince",
+      class: "",
+    },
+    {
+      title: "Python With AI ML",
+      icon: "ion-ios-world-outline",
+      color: "#3fcdc7",
+      ref: 17293,
+      exp: "5 - 7",
+      type: "Permanent",
+      location: "Pune",
+      department: "Services",
+      class: "offset-lg-1",
+    },
+    {
+      title: "Java Full Stack Developer",
+      icon: "ion-social-android-outline",
+      color: "#41cf2e",
+      ref: 787293,
+      exp: "3 - 5",
+      type: "Permanent",
+      location: "Pune",
+      department: "Development",
+      class: "",
+    },
+    {
+      title: "Dot Net Developer",
+      icon: "ion-ios-world-outline",
+      color: "#d6ff22",
+      ref: 287383,
+      exp: "3 - 5",
+      type: "Permanent",
+      location: "Pune",
+      department: "Development",
+      class: "offset-lg-1",
+    },
+    {
+      title: "Business Analyst",
+      icon: "ion-stats-bars",
+      color: "#4680ff",
+      ref: 387293,
+      exp: "3 - 5",
+      type: "Permanent",
+      location: "Pune",
+      department: "Services",
+      class: "",
+    },
+  ];
+  return (
+    <>
+      {JobData.map(function (obj, ind) {
+        return (
+          <div
+            className={`col-md-6 col-lg-5 ${obj.class} wow bounceInUp`}
+            data-wow-duration="1.4s"
+            key={ind}
+          >
+            <div className="box">
+              <div className="icon">
+                <i className={obj.icon} style={{ color: obj.color }}></i>
+              </div>
+
+              <h4 className="title">
+                <a href="">{obj.title}</a>
+              </h4>
+              <p className="description">
+                <ul style={{ listStyleType: "disc" }}>
+                  <li>Ref:{obj.ref}</li>
+                  <li>Experience: {obj.exp} years</li>
+                  <li>Contract type: {obj.type}</li>
+                  <li>location : {obj.location} </li>
+                  <li>Department:{obj.department}</li>
+                </ul>
+                &nbsp; &nbsp; &nbsp; &nbsp;{" "}
+                <a href="Under_Maintenance.html">
+                  {" "}
+                  <button className="button button100">Apply Now</button>
+                </a>
+              </p>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
 const Clients = () => {
   const ClientList = [
     Client1,
@@ -20,19 +150,19 @@ const Clients = () => {
     Client8,
   ];
   return (
-    <section id="clients" className="wow fadeIn">
-      <div className="container">
-        <div className="section-header">
+    <section id="clients" classNameName="wow fadeIn">
+      <div classNameName="container">
+        <div classNameName="section-header">
           <h3>Our Clients</h3>
         </div>
 
-        <div className="row no-gutters clients-wrap clearfix wow fadeInUp">
+        <div classNameName="row no-gutters clients-wrap clearfix wow fadeInUp">
           {ClientList.map(function (image, i) {
             return (
               <>
-                <div className="col-lg-3 col-md-4 col-xs-6">
-                  <div className="client-logo">
-                    <img src={image} className="img-fluid" alt="Client" />
+                <div classNameName="col-lg-3 col-md-4 col-xs-6" key={i}>
+                  <div classNameName="client-logo">
+                    <img src={image} classNameName="img-fluid" alt="Client" />
                   </div>
                 </div>
               </>
